54f96ae8314839f9f525be7d9310dd3441ac1d44
Tao Ning <ningtaous@gmail.com>
2016-11-11 10:53:05 -0500

    [feat] Add filter to search meeting room

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -4,10 +4,9 @@ export default Ember.Controller.extend({
   actions: {
     filterByName(param) {
       if (param !== '') {
-        return this.get('store').findAll('meetingroom');
+        return this.get('store').query('meetingroom', { name: param });
       } else {
         return this.get('store').findAll('meetingroom');
       }
     }
-  }
-});
+  }});
