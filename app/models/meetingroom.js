19f8bc8a007c499a64934fb86326d2b3970c12c5
Tao Ning <ningtaous@gmail.com>
2016-11-11 10:54:09 -0500

    [feat] Add toggle image to mark meeting room

--- app/models/meetingroom.js
+++ app/models/meetingroom.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   "name": DS.attr(),
   "position-x": DS.attr(),
   "position-y": DS.attr(),
-  "image": DS.attr()
+  "image-1": DS.attr(),
+  "image-2": DS.attr()
 });
